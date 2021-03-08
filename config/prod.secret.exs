a73752c6e6e80fe28e2534df3850a1274a2c9e88
Nathan Kuik <nkuik@toogoodtogo.com>
2021-03-08 20:00:21 +0100

    Add necessary buildpack components

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,8 +12,8 @@ database_url =
     """
 
 config :hyf, Hyf.Repo,
-  # ssl: true,
-  url: database_url,
+  ssl: true,
+  # url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 secret_key_base =
